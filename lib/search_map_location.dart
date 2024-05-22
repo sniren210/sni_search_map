44947bee15b47a3ed4e2fac6d50db242bf744d13
sniren210 <sniren2002@gmail.com>
2024-05-22 11:12:02 +0700

    fix: export

--- lib/search_map_location.dart
+++ lib/search_map_location.dart
@@ -1,4 +1,8 @@
 library sni_search_map;
 
 export 'package:sni_search_map/widget/search_widget.dart';
-
+export 'package:sni_search_map/utils/google_search/geo_coding.dart';
+export 'package:sni_search_map/utils/google_search/geo_location.dart';
+export 'package:sni_search_map/utils/google_search/latlng.dart';
+export 'package:sni_search_map/utils/google_search/place_type.dart';
+export 'package:sni_search_map/utils/google_search/place.dart';
